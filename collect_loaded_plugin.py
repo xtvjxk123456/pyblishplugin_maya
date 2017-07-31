f655493ec030b66170181f0f13fe3e190733b6d2
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-07-31 13:45:08 +0800

    [Fix]修改注释

--- collect_loaded_plugin.py
+++ collect_loaded_plugin.py
@@ -10,9 +10,11 @@ class CollectLoadedPlugin(pyblish.api.ContextPlugin):
     label = "Current Loaded Plugin"
     # hosts = ['maya']
     # 只有host可以显示在pyblish qml界面上,host 可以是列表
-    hosts = ['maya','mayapy']
+    # 这是因为在maya里运行的时候,本质pyblish_qml的host是python
+    # 需要手动注册,不能使用环境变量方式
+    hosts = ['maya', 'mayapy']
 
     def process(self, context):
-        loadedPluginPath =map(lambda x:pm.Path(x).basename().name,pm.pluginInfo(q=True,lsp=True))
+        loadedPluginPath = map(lambda x: pm.Path(x).basename().name, pm.pluginInfo(q=True, lsp=True))
         context.data['LoadedPlugins'] = loadedPluginPath
         # self.log.info('current pyblish_qml host is {}'.format(pyblish.api.current_host()))
